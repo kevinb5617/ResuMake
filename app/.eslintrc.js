b0cd76e721a16ad8c5bdadee84babd4ccb1984b7
Kevin Boyle <43416546+kevinb5617@users.noreply.github.com>
2021-04-14 12:06:17 -0400

    Create eslint action (#18)
    * Removed extra commits
    
    * Create eslint.yml
    
    * Fixed all eslint issues in master

---
+++ app/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    semi: 0,
+    'linebreak-style': 0,
+    'react/react-in-jsx-scope': 0,
+  },
+};
---
+++ app/src/components/Navigation/Toolbar/Toolbar.jsx
@@ -0,0 +1,10 @@
+import Logo from '../../Logo/Logo'
+import classes from './Toolbar.module.css'
+
+const Toolbar = () => (
+  <header className={classes.Toolbar}>
+    <Logo />
+  </header>
+)
+
+export default Toolbar;
