353158040037b9589ba2b1c3d8d0713e205ef622
Kevin Boyle <43416546+kevinb5617@users.noreply.github.com>
2021-04-14 13:40:32 -0400

    Create "Select Pages" page (#13)
    * Added SelectPages starter code
    
    * Styled checkboxes for adding pages
    
    * Added useState to store page choices
    
    * Fixed linting errors and scrollbar

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,25 @@
 /* eslint-disable react/jsx-filename-extension */
 import './App.css'
-import Layout from './components/Layout/Layout'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Homepage from './components/Homepage/Homepage'
+import Toolbar from './components/Navigation/Toolbar/Toolbar'
+import SelectPages from './components/SelectPages/SelectPages'
 
 const App = () => (
-  <div>
-    <Layout />
-  </div>
+  <>
+    <Toolbar />
+    <Router>
+      <Route
+        path="/"
+        exact
+        component={Homepage}
+      />
+      <Route
+        path="/select-pages"
+        component={SelectPages}
+      />
+    </Router>
+  </>
 )
 
 export default App
