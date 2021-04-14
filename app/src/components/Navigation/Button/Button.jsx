27214cdf68c8b205e2c101e61e62c690a6fb20e6
Kevin Boyle <kevincba17@gmail.com>
2021-04-14 14:08:57 -0400

    Fixed merge issue

--- app/src/components/Navigation/Button/Button.jsx
+++ app/src/components/Navigation/Button/Button.jsx
@@ -2,25 +2,32 @@ import { Link } from 'react-router-dom'
 import classes from './Button.module.css'
 
 const Button = ({ nextPage, type, text }) => {
+  let returnLink
+
   switch (type) {
     case 'next':
-      return (
+      returnLink = (
         <Link to={nextPage}>
           <button type="button" className={`${classes.btn} ${classes.next_btn}`}>
             {text}
           </button>
         </Link>
       )
+      break
     case 'back':
-      return (
+      returnLink = (
         <Link to={nextPage}>
           <button type="button" className={`${classes.btn} ${classes.back_btn}`}>
             {text}
           </button>
         </Link>
       )
+      break
     default:
+      returnLink = null
   }
+
+  return returnLink
 }
 
 export default Button
